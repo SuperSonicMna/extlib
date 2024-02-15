d78635d5a90cf77029451d9a7676b9686fcac1e4
atrexus <randomusernameemail2021@gmail.com>
2024-02-15 00:34:01 -0500

    iterator pt.2

--- extlib/src/win/snapshot.cpp
+++ extlib/src/win/snapshot.cpp
@@ -4,5 +4,6 @@ namespace extlib::win
 {
     extlib::win::snapshot< snapshot_kind::process_t >::iterator::iterator( std::shared_ptr< handle_t > handle, bool first )
     {
+
     }
 }  // namespace extlib::win
